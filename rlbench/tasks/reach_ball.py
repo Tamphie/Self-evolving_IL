2ad49b76bc06404660c6d1556b048b6d8d979162
Tamphie <xtongmiao@gmail.com>
2024-09-26 07:08:43 +0000

    record_demo_v1

---
+++ rlbench/tasks/reach_ball.py
@@ -0,0 +1,20 @@
+from typing import List
+from rlbench.backend.task import Task
+from typing import Tuple
+
+class ReachBall(Task):
+
+    def init_task(self) -> None:
+        # TODO: This is called once when a task is initialised.
+        pass
+
+    def init_episode(self, index: int) -> List[str]:
+        # TODO: This is called at the start of each episode.
+        return ['']
+
+    def variation_count(self) -> int:
+        # TODO: The number of variations for this task.
+        return 1
+
+    def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
+        return [0, 0, 0], [0, 0, 0]
---
+++ scripts/generate_dataset_IL.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 rlbench/dataset_generator.py --tasks open_box --variations 1
